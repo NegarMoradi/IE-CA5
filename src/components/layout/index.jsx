0f1ca3bce082f8919a07daad289ef43768a35dc2
Alireza Rostami <rostami.alireza@digikala.com>
2023-05-01 11:30:48 +0330

    add layout

---
+++ src/components/layout/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Routing from '../../routing';
+
+const Layout = ({children}) => {
+    return (
+        <div>
+            <Routing />
+        </div>
+    )
+}
+
+export default Layout
