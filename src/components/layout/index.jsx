f2d6f7c95552365ca0c6f5ef56d4e5551a885d30
Alireza Rostami <rostami.alireza@digikala.com>
2023-05-03 23:33:34 +0330

    create layout

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Routing from '../../routing';
+import Footer from './footer';
+import Header from './header';
 
 const Layout = ({children}) => {
     return (
         <div>
+            <Header />
             <Routing />
+            <Footer />
         </div>
     )
 }
