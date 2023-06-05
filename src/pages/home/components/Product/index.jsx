4ba4835010f53d8e78d974511631f1cc93285ee9
negar <negar.moradi@digikala.com>
2023-06-05 14:42:47 +0330

    fix: image

--- src/pages/home/components/Product/index.jsx
+++ src/pages/home/components/Product/index.jsx
@@ -1,6 +1,6 @@
 
 import './product.css';
-import image from '../../../../assets/png/mobile.png';
+
 import commafy from '../../../../components/commafy';
 
 const Product = ({product}) => {
@@ -9,7 +9,7 @@ const Product = ({product}) => {
             <div class="item pt-3">
                 <p class="item-title">{product.name}</p>
                 <p class="item-count">{product.inStock} left in stock</p>
-                <img class="w-100" src={image} alt="item"/>
+                <img src={product.image} alt="item"/>
                 <div class="item-details d-flex justify-content-between align-items-center m-2">
                     <p class="mb-0">{commafy(product.price)}$</p>
                     <button disabled={!product.inStock}>add to cart</button>
